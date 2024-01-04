c438eb640d2a3add62440332e14ec24c3b8a53e2
sanimations <smartucci157@gmail.com>
2024-01-03 21:07:42 -0500

    Updated App.js with FirebaseConfig and fixed Stack.screen

--- App.js
+++ App.js
@@ -1,29 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Start from './components/Start';
-import Chat from './components/Chat';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Start from "./components/Start";
+import Chat from "./components/Chat";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyC2I5ED5hBs1N76ND9DnEalAHcevS6-_XY",
+    authDomain: "walkintalkin-b9f0b.firebaseapp.com",
+    projectId: "walkintalkin-b9f0b",
+    storageBucket: "walkintalkin-b9f0b.appspot.com",
+    messagingSenderId: "913148977162",
+    appId: "1:913148977162:web:9f762344868d2399fbbff3",
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  // Initialize Cloud Firestore and get a reference to the service
+  const db = getFirestore(app);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Start"
-      >
-        <Stack.Screen
-          name="Start"
-          component={Start}
-        />
-        <Stack.Screen
-          name="Chat"
-          component={Chat}
-        />
+      <Stack.Navigator initialRouteName="Start">
+        <Stack.Screen name="Start" component={Start} />
+        <Stack.Screen name="Chat">
+          {(props) => <Chat db={db} {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
