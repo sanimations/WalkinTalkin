fedd232a860f99744b492d9d5c3ab93b0bdb96b5
sanimations <smartucci157@gmail.com>
2024-01-08 03:19:10 -0500

    Updated chat page to function when offline with Alerts and AsyncStorage

--- components/Chat.js
+++ components/Chat.js
@@ -6,7 +6,7 @@ import {
   KeyboardAvoidingView,
   Platform,
 } from "react-native";
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import {
   collection,
   addDoc,
@@ -15,21 +15,36 @@ import {
   orderBy,
 } from "firebase/firestore";
 
-const Chat = ({ route, navigation, db }) => {
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const Chat = ({ route, navigation, db, isConnected }) => {
   const { name, userID, bgColor } = route.params;
 
   const [messages, setMessages] = useState([]);
 
+  let unsubMessages;
+
   useEffect(() => {
+
     navigation.setOptions({ title: name });
+
+    if(isConnected === true) {
+
+      //unsubscribe if already subscribed
+      if(unsubMessages) unsubMessages();
+      unsubMessages = null;
+
+
+      //Firestore query to get messages sorted by most recently created/sent
     const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-    const unsubMessages = onSnapshot(q, (documentsSnapshot) => {
+      unsubMessages = onSnapshot(q, (documentsSnapshot) => {
       let newMessages = [];
 
       const currentUserID = route.params.userID;
       documentsSnapshot.forEach((doc) => {
         const messageData = doc.data();
 
+        //Only include messages from the current user
         if (messageData.user._id === currentUserID) {
           newMessages.push({
             id: doc.id,
@@ -38,19 +53,41 @@ const Chat = ({ route, navigation, db }) => {
           });
         }
       });
+      //cache the new messages and update the useState
+      cacheMessages(newMessages);
       setMessages(newMessages);
     });
+  }else loadCachedMessages();  //When offline
 
     // Clean up code
     return () => {
       if (unsubMessages) unsubMessages();
     };
-  }, []);
+  }, [isConnected]);
 
-  // Caches messages sent
+  const loadCachedMessages = async () => {
+    const cachedMessages = await AsyncStorage.getItem("messages") || [];
+      setMessages(JSON.parse(cachedMessages));
+   }
+
+   const cacheMessages = async (messagesToCache) => {
+    try {
+      await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  // Send a new message to Firestore
   const onSend = async (newMessages) => {
     addDoc(collection(db, "messages"), newMessages[0]);
   };
+
+  // render toolbar based on connection status
+  const renderInputToolbar = (props) => {
+    if (isConnected) return <InputToolbar {...props} />;
+    else return null;
+   }
 
   // Change Bubble colors
   const renderBubble = (props) => {
@@ -74,6 +111,7 @@ const Chat = ({ route, navigation, db }) => {
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
         onSend={(messages) => onSend(messages)}
         user={{
           _id: userID,
